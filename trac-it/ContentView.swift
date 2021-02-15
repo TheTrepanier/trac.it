bd2b0b64d66a784a12cd1acf42c105ffbf95c3e1
TheTrepanier_Dev <TheTrepanier@icloud.com>
2021-02-15 15:51:21 -0700

    init commit

---
+++ trac-it/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  trac-it
+//
+//  Created by Michael Trepanier on 2/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ trac-it/trac_itApp.swift
@@ -0,0 +1,17 @@
+//
+//  trac_itApp.swift
+//  trac-it
+//
+//  Created by Michael Trepanier on 2/15/21.
+//
+
+import SwiftUI
+
+@main
+struct trac_itApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
